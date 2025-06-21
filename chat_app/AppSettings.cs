f271c990ac79b612329e2f4ae4c0e17e58c6f3b1
Harish-Madhavan <veeharish2005@gmail.com>
2025-06-21 14:38:57 +0530

    initial code

---
+++ chat_app/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿// AppSettings.cs
+namespace LlamaChatApp
+{
+    public class AppSettings
+    {
+        public string? LastModelPath { get; set; }
+        public double Temperature { get; set; } = 0.7;
+        public double TopP { get; set; } = 0.9;
+        public int MaxTokens { get; set; } = 512;
+    }
+}
